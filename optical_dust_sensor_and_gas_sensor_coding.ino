4f9516f12acc929c82ddc374813d311c22e81c2a
Shamik <64892076+shazm12@users.noreply.github.com>
2020-09-11 01:20:17 +0530

    Added the source file

---
+++ optical_dust_sensor_and_gas_sensor_coding.ino
@@ -0,0 +1,173 @@
+String myAPIkey = "Your Write API key here";  
+
+#include <SoftwareSerial.h>
+
+SoftwareSerial ESP8266(2, 3); // Rx,  Tx
+
+
+int measurePin = A0;
+int ledPower = 12;
+
+unsigned int samplingTime = 280;
+unsigned int deltaTime = 40;
+unsigned int sleepTime = 9680;
+long writingTimer = 17;
+long startTime=0;
+long waitTime=0;
+
+float voMeasured = 0;
+float calcVoltage = 0;
+float dustDensity = 0;
+int mq2 = A1;
+int data=0;
+int percent=0;
+
+boolean relay1_st = false; 
+boolean relay2_st = false; 
+unsigned char check_connection=0;
+unsigned char times_check=0;
+boolean error;
+
+
+
+
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(ledPower,OUTPUT); 
+  ESP8266.begin(9600); 
+  startTime = millis(); 
+  ESP8266.println("AT+RST");
+  delay(2000);
+  Serial.println("Connecting to Wifi");
+   while(check_connection==0)
+  {
+    Serial.print(".");
+  ESP8266.print("AT+CWJAP=\"Wifi Network's name\",\"PassWord\"\r\n");
+  ESP8266.setTimeout(5000);
+ if(ESP8266.find("WIFI CONNECTED\r\n")==1)
+ {
+ Serial.println("WIFI CONNECTED");
+ break;
+ }
+ times_check++;
+ if(times_check>3) 
+ {
+  times_check=0;
+   Serial.println("Trying to Reconnect..");
+  }
+  }
+}
+
+void loop()
+{
+  waitTime = millis()-startTime;   
+  if (waitTime > (writingTimer*1000)) 
+  {
+    dustsensor();
+    gassensor();
+    writeThingSpeak();
+    startTime = millis();   
+  }
+}
+void dustsensor(){
+  digitalWrite(ledPower,LOW);
+  delayMicroseconds(samplingTime);
+
+  voMeasured = analogRead(measurePin);
+
+  delayMicroseconds(deltaTime);
+  digitalWrite(ledPower,HIGH);
+  delayMicroseconds(sleepTime);
+
+  calcVoltage = voMeasured*(5.0/1024);
+  dustDensity = 0.17*calcVoltage-0.1;
+
+  if ( dustDensity < 0)
+  {
+    dustDensity = 0.00;
+  }
+
+  Serial.println("Raw Signal Value (0-1023):");
+  Serial.println(voMeasured);
+
+  Serial.println("Voltage:");
+  Serial.println(calcVoltage);
+
+  Serial.println("Dust Density:");
+  Serial.println(dustDensity);
+
+  delay(1000);
+}
+void gassensor() {
+  data=analogRead(mq2);
+  Serial.println("Air pollution level:");
+  percent=(data/800)*100;
+  Serial.println(percent);
+}
+
+
+
+
+void writeThingSpeak(void)
+{
+  startThingSpeakCmd();
+  // preparacao da string GET
+  String getStr = "GET /update?api_key=";
+  getStr += myAPIkey;
+  getStr +="&field1=";
+  getStr += String(dustDensity);
+  getStr +="&field2=";
+  getStr += String(percent);
+  getStr += "\r\n\r\n";
+  GetThingspeakcmd(getStr); 
+}
+
+void startThingSpeakCmd(void)
+{
+  ESP8266.flush();
+  String cmd = "AT+CIPSTART=\"TCP\",\"";
+  cmd += "184.106.153.149"; // api.thingspeak.com IP address
+  cmd += "\",80";
+  ESP8266.println(cmd);
+  Serial.print("Start Commands: ");
+  Serial.println(cmd);
+
+  if(ESP8266.find("Error"))
+  {
+    Serial.println("AT+CIPSTART error");
+    return;
+  }
+}
+
+String GetThingspeakcmd(String getStr)
+{
+  String cmd = "AT+CIPSEND=";
+  cmd += String(getStr.length());
+  ESP8266.println(cmd);
+  Serial.println(cmd);
+
+  if(ESP8266.find(">"))
+  {
+    ESP8266.print(getStr);
+    Serial.println(getStr);
+    delay(500);
+    String messageBody = "";
+    while (ESP8266.available()) 
+    {
+      String line = ESP8266.readStringUntil('\n');
+      if (line.length() == 1) 
+      { 
+        messageBody = ESP8266.readStringUntil('\n');
+      }
+    }
+    Serial.print("MessageBody received: ");
+    Serial.println(messageBody);
+    return messageBody;
+  }
+  else
+  {
+    ESP8266.println("AT+CIPCLOSE");     
+    Serial.println("AT+CIPCLOSE"); 
+  } 
+}
